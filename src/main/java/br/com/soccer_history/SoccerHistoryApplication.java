6a4b4eee789043294c90fdabb86a83ac1c9c0e62
FelipeAriki <felipeariki3@gmail.com>
2025-08-31 21:47:54 -0300

    Initial commit

---
+++ src/main/java/br/com/soccer_history/SoccerHistoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.soccer_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/soccer_history/SoccerHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soccer_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
